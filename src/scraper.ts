b2810a4fd76af933e797fcfdadd0653e534e5bd6
Carissa Cecil <carissacecil@gmail.com>
2022-08-01 05:56:30 -0700

    clean up comments

--- src/scraper.ts
+++ src/scraper.ts
@@ -11,11 +11,11 @@ export interface ListingData {
 }
 
 async function getPropertyList(): Promise<ListingData[]> {
-  const url = 'https://ccecil-scraper.herokuapp.com/properties'; // URL we're scraping
+  const url = 'https://ccecil-scraper.herokuapp.com/properties'; // Backend service url to scrape and retrieve property listings
   const AxiosInstance = axios.create(); // Create a new Axios Instance
   let propertyResultList: ListingData[] = []  
 
-    // Send an async HTTP Get request to the url
+  // Send an async HTTP Get request to the url
   const response = await AxiosInstance.get(url);
   propertyResultList = response.data; 
   return propertyResultList;
